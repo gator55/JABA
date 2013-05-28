0bb93d300b0123edba5a79a1d0ac226bcce8a548
GatoR <gator@bigmir.net>
2013-05-28 19:43:00 +0300

    hgfhf

---
+++ HomeWork1/src/Supp.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: olp
+ * Date: 5/28/13
+ * Time: 7:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Supp {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+
+        int i;
+
+        for(i=0;i==5;i++){
+            System.out.println(i);
+            break;
+        }
+    }
+
+}
