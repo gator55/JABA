23a639605d70303b4945c04ba815107ac7e1dd9c
GatoR <gator@bigmir.net>
2013-05-28 19:30:18 +0300

    test1

---
+++ untitled/src/Supp.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: olp
+ * Date: 5/28/13
+ * Time: 7:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Supp {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+
+        for(;;){
+            break;
+        }
+    }
+
+}
