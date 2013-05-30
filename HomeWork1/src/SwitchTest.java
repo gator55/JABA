1df423122048afc4850fcacbdb1da10ff4b5dd4b
GatoR <gator@bigmir.net>
2013-05-30 18:14:24 +0300

    first

--- HomeWork1/src/SwitchTest.java
+++ HomeWork1/src/SwitchTest.java
@@ -5,45 +5,61 @@
  * Time: 5:38 PM
  * To change this template use File | Settings | File Templates.
  */
-import sun.org.mozilla.javascript.internal.ast.WhileLoop;
 
 import java.io.*;
 
 public class SwitchTest {
     public static void main(String[] args) {
-        int x=0;
-        boolean again=false;
+        int x = 0;
+        String new_val="n";
+        boolean start_new = true;
 
-        while (again){
+        while (start_new) {
 
-        System.out.println("Please enter x:");
+            System.out.println("Please enter \"x\" (number):");
+            try {
+                BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+                String s = bufferRead.readLine();
 
-        try{
-            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-            String s = bufferRead.readLine();
+                try{
+                    x = Integer.parseInt(s);
+                } catch (NumberFormatException e) {
+                    System.out.println("It's not a number! Bye...");
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-            System.out.println(s);
+            switch (x) {
+                case 0: {
+                    System.out.println("case 0");
+                }
+                case 1: {
+                    System.out.println("case 1, break");
+                    break;
+                }
+                case 2: {
+                    System.out.println("case 2");
+                }
+                default: {
+                    System.out.println("case default");
+                }
+            }
+
+            System.out.println("Enter new value for \"x\"?[y/n]:");
+            try {
+                BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+                new_val = bufferRead.readLine();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (!new_val.equalsIgnoreCase("y")) {
+                start_new=false;
+                System.out.println("Bye!");
+            }
         }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        switch (x){
-            case 0: {
-                System.out.println("0");
-            }
-            case 1: {
-                System.out.println("1");
-                break;
-            }
-            case 2:{
-                System.out.println("2");
-            }
-            default:{
-                System.out.println("default");
-            }
-        }
-    }
     }
 }
