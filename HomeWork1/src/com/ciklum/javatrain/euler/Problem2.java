892e7ad701313e2a89f724de98c45f62e5a13d53
GatoR <gator@bigmir.net>
2013-06-05 18:54:52 +0300

    first

--- HomeWork1/src/com/ciklum/javatrain/euler/Problem2.java
+++ HomeWork1/src/com/ciklum/javatrain/euler/Problem2.java
@@ -6,11 +6,34 @@ package com.ciklum.javatrain.euler;
  * Date: 6/3/13
  * Time: 2:33 PM
  * To change this template use File | Settings | File Templates.
- *
+ * <p/>
  * Even Fibonacci numbers
+ * under 4,000,000
+ * sum of even numbers
  */
 public class Problem2 {
-    public static void main(String[] args) {
+    private static int fib = 1;
 
+    public static void main(String[] args) {
+        int max = 4000000;
+        int start = 1;
+        int fib_sum = 0;
+
+        while (true) {
+            start = fib - start;
+            fib += start;
+
+            if (fib >= max){
+                break;
+            }
+            System.out.println("start = " + start + ",fib = " + fib);
+
+            if(fib%2 == 0){
+                fib_sum += fib;
+            }
+        }
+
+        System.out.println(fib_sum);
     }
+
 }
---
+++ HomeWork1/src/com/ciklum/javatrain/xo/Field.java
@@ -0,0 +1,11 @@
+package com.ciklum.javatrain.xo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: olp
+ * Date: 6/5/13
+ * Time: 6:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Field {
+}
